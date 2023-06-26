2d89842d3508378ac4d08a9b2f37d5de91fd092f
Sheldon Zheng <zhen371@usc.edu>
2023-06-26 19:17:27 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7766995a39f54deaa1fc8728d854c341',
+  rootNotionPageId: '7666d450f2be42639b614c12b0543369',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,16 +10,16 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Sheldon的blog',
-  domain: 'https://www.notion.so/sheldon123z/Next-js-Notion-Starter-Kit-Template-7766995a39f54deaa1fc8728d854c341?pvs=4',
+  domain: 'https://www.notion.so/sheldon-zheng/Sheldon-Blog-7666d450f2be42639b614c12b0543369?pvs=4',
   author: 'Sheldon Zheng',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: '',
+  linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
